bbcd748094b9da89f8149541adec2015da12800d
xinyuma0214 <xinyuma@umich.edu>
2021-08-26 12:52:12 -0400

    add id and secret

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 // import express from 'express';
+const passport = require('passport');
+const GoogleStrategy  = require('passport-google-oauth20').Strategy;
+const keys  =require('./config/keys')
 const app = express();
+// app.get('/',(req,res)=>{
+//   res.send({ bye:'buddy' });
+// });
+passport.use(new GoogleStrategy({
+  cliendID: keys.googleusercontent,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+},
+  accessToken =>{
+    console.log(accessToken);
+  }
+));
 
-app.get('/',(req,res)=>{
-  res.send({ bye:'buddy' });
-});
+app.get('/auth/google', passport.authenticate('google',{
+  scope: ['profile','email']
+})
+);
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
